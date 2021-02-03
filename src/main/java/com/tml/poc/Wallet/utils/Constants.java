d278ab0c8c89d3af25d1a308d83d542331bbfd62
PratikBanodkar <pratikbanodkar@gmail.com>
2021-02-03 11:31:25 +0530

    Merge conflict resolved

--- src/main/java/com/tml/poc/Wallet/utils/Constants.java
+++ src/main/java/com/tml/poc/Wallet/utils/Constants.java
@@ -2,9 +2,25 @@ package com.tml.poc.Wallet.utils;
 
 public class Constants {
 
-	
+	/** generic Constants
+	 */
 	public static final String EMAIL_REGEX="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
 	public static final String TIME_DATE="yyyy-MM-dd'T'HH:mm:ss Z";
+
+	/**
+	 * security
+	 */
+	public static final int SALT_COUNT=128;
+
+
+	/**
+	 * transactional Constants
+	 */
+	public static final String INSUFFICIENT_BALANCE="Insufficient Balance";
+	public static final String TRANSACTION_FAILED="Transaction Getting Failed";
+
+
+	public static final String TRANSACTION_COMPLETED_SUCCESSFULLY = "Transaction Completed Successfully";
 
 	public static final String FCM_ADMIN_SDK_JSON_PATH = "google/tml-wallet-firebase-adminsdk.json";
 	public static final String FCM_UNICAST_SUCCESS_MESSAGE = "Sent Unicast message to token. Received response : ";
